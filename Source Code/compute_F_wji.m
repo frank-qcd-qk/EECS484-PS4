dfa407027cda90862e659e041a9d17c1c8999727
frank-qcd-qk <cxq41@case.edu>
2018-10-30 18:57:06 -0400

    Final Commitment and submission

--- Source Code/compute_F_wji.m
+++ Source Code/compute_F_wji.m
@@ -1,17 +1,20 @@
 %ordered derivatives with respect to wji
-function [F_wji] = compute_F_wji(sigma_history,F_u)
-temp = size(F_u);
-Nneurons = temp(1);
-T_time_steps=temp(2);
-F_wji = zeros(Nneurons,Nneurons); %create holder for output with correct dimensions
+function [F_wji] = compute_F_wji(sigma_history, F_u)
+    temp = size(F_u);
+    Nneurons = temp(1);
+    T_time_steps = temp(2);
+    F_wji = zeros(Nneurons, Nneurons); %create holder for output with correct dimensions
 
-%compute sum of F_uvals terms over time:
-%DO SOMETHING REAL HERE
-%Fixed
-for j = 1:Nneurons
-    for i = 1:Nneurons
-        for t = 2:(T_time_steps)
-            F_wji(j,i) = F_wji(j,i)+F_u(j,t)*sigma_history(i,t-1);
+    %compute sum of F_uvals terms over time:
+    %Fixed
+    for j = 1:Nneurons
+
+        for i = 1:Nneurons
+
+            for t = 2:(T_time_steps)
+                F_wji(j, i) = F_wji(j, i) + F_u(j, t) * sigma_history(i, t - 1);
+            end
+
         end
+
     end
-end
